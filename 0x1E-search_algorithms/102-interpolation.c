ece063113410ca42823151128613b400bf100b61
oussama El mazali <132847557+BANKLORD@users.noreply.github.com>
2024-03-12 11:51:36 +0000

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,44 +1,52 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - searches for a value in a sorted array of integers
- * using the Interpolation search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in the array
- * @value: value to search for
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
  *
- * Return: the index where the value is located, or -1 if value is not present
- * in array or if array is NULL
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, pos = 0;
-	int i;
+	size_t pos, low, high;
+	double f;
 
-
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
-	for (i = 0; low <= high && value >= array[low] && value <= array[high]; i++)
-	{
-		pos = low + (((double)(high - low) / (array[high] - array[low]))
-		* (value - array[low]));
+	low = 0;
+	high = size - 1;
 
-		printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
+	while (size)
+	{
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
 
 		if (array[pos] == value)
-			return (pos);
-		else if (array[pos] > value)
-		{
-			high = pos - 1;
-		}
-		else if (array[pos] < value)
-		{
+			return ((int)pos);
+
+		if (array[pos] < value)
 			low = pos + 1;
-		}
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
 
-	printf("(Value checked array[%d] is out of range)\n", array[i]);
 	return (-1);
 }
